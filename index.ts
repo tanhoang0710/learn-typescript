d4344c0178618482ecf06b0fcd769f117bd7a94c
tanhoang0710 <tanhoang0710@gmail.com>
2022-08-17 11:48:00 +0700

    03-04 Tất tần tật về Enum trong typescript

--- index.ts
+++ index.ts
@@ -1,5 +1,53 @@
-// Lưu ý khi làm việc với function
-// return type
-let sum = (x: number, y: number): number => {
-    return x + y;
-};
+// Enum (default là number enum)
+// 1. Number enum
+
+// default từ 0
+// enum Status {
+//     PENDING,      // 0
+//     IN_PROGRESS,  // 1
+//     DONE,         // 2
+//     CANCELED      // 3
+// }
+
+// bắt đầu từ 3
+// enum Status {
+//     PENDING = 3, // 3
+//     IN_PROGRESS, // 4
+//     DONE, // 5
+//     CANCELED, // 6
+// }
+
+// tự config:
+enum Status {
+    PENDING = 3,
+    IN_PROGRESS = 6,
+    DONE = 8,
+    CANCELED = 10,
+}
+
+// có thể gán giá trị bất kì cho enum variable
+const stats1: Status = Status.PENDING;
+const stats2: Status = 1;
+const stats3: Status = -1;
+
+// number enum --> support reverse mapping
+console.log(Status);
+console.log(Status[3]); // PENDING
+console.log(Status['DONE']); // 8
+console.log(stats3); // 8
+
+// 2. String enum
+enum MediaTypes {
+    JSON = 'application/json',
+    XML = 'application/xml',
+}
+
+fetch('https://example.com/api/endpoint', {
+    headers: {
+        Accept: MediaTypes.JSON,
+    },
+}).then((res) => res.json());
+
+// 3. Khi nào dùng enum
+// - Khi nào dùng dữ liệu 1 chiều
+// - Khi dữ liệu API trả về ko dùng enum, mà dùng union type
