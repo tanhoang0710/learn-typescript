57a4619207cf3a618ae2184e540ca814c18a8f6d
tanhoang0710 <tanhoang0710@gmail.com>
2022-08-23 14:24:10 +0700

    03-06 Một số Utility Types phổ biến

--- index.ts
+++ index.ts
@@ -1,33 +1,63 @@
-// Generics
-// Hiểu nôm na là kiểu dữ liệu mà có nhận tham số và trả về kiểu dữ liệu tương ứng
+// Utility Types
+// Partial<Type> - ko can du cac key
 
-interface Student {
-    id: number;
-    name: string;
+interface Todo {
+    title: string;
+    description: string;
 }
 
-const numberList: Array<number> = [1, 2, 3];
-const wordList: Array<string> = ['easy', 'frontend'];
-
-const studentList: Array<Student> = [
-    { id: 1, name: 'Alice' },
-    { id: 2, name: 'Bob' },
-];
-
-// VD:
-interface List<T> {
-    length: number;
-    [index: number]: T;
+function updateTodo(todo: Todo, fieldsToUpdate: Partial<Todo>) {
+    return { ...todo, ...fieldsToUpdate };
 }
 
-const numberList1: List<number> = [1, 2, 3];
-const wordList1: List<string> = ['easy'];
+const todo1 = {
+    title: 'organize desk',
+    description: 'clear clutter',
+};
 
-const studentList1: List<Student> = [
-    { id: 1, name: 'Alice' },
-    { id: 2, name: 'Bob' },
-];
+const todo2 = updateTodo(todo1, {
+    description: 'throw out trash',
+});
 
-type StudentKeys = keyof Student;
-// danh sach key cua Student
-const keys: StudentKeys = 'id';
+// Required<Type> - nguoc voi Partial, can du cac key
+
+// Readonly<Type> - ko assign duoc sau khi init
+
+// Record<Keys, Type>
+type CatName = 'miffy' | 'boris' | 'mordred';
+
+interface CatInfo {
+    age: number;
+    breed: string;
+}
+
+const cats: Record<CatName, CatInfo> = {
+    miffy: { age: 10, breed: 'Persian' },
+    boris: { age: 5, breed: 'Maine Coon' },
+    mordred: { age: 16, breed: 'British Shorthair' },
+};
+
+console.log(cats);
+
+// Pick<Type, Keys> - định nghĩa 1 kiểu dữ liệu mới dựa trên việc pick các key từ kiểu dữ liệu cũ
+interface Todo1 {
+    title: string;
+    description: string;
+    completed: boolean;
+}
+
+type TodoPreview = Pick<Todo1, 'title' | 'completed'>;
+
+const todo: TodoPreview = {
+    title: 'clean room',
+    completed: false,
+};
+
+console.log(todo);
+
+// Omit<Type, Keys> - nguoc lai voi Pick - chi lay những key, trừ những key
+
+// ReturnType<Type> - Lấy kiểu trả về của 1 hàm
+declare function f1(): { a: number; b: string };
+
+type T0 = ReturnType<() => string>;
